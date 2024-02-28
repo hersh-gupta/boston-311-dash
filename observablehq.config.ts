c803efe8665248704b2a201cdfd2cc6bca591fb0
Hersh Gupta <h.v.gupta@outlook.com>
2024-02-27 20:04:47 -0500

    header

--- observablehq.config.ts
+++ observablehq.config.ts
@@ -19,6 +19,13 @@ export default {
   // Some additional configuration options and their defaults:
   // theme: "default", // try "light", "dark", "slate", etc.
   // header: `<div>GitHub</div>`,
+  head: `<link rel="icon" type="image/vnd.microsoft.icon" href="https://raw.githubusercontent.com/hersh-gupta/boston-311-dash/main/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="description" content="Explore Boston's 311 data">
+  <meta property="og:type" content="website">
+  <meta property="og:title" content="Boston 311 Dashboard">
+  <meta property="og:description" content="Explore Boston's 311 data">
+`,
   footer: `Built by <a href="https://github.com/hersh-gupta">Hersh Gupta</a> using the <a href="https://observablehq.com/">Observable Framework</a>.`, // what to show in the footer (HTML)
   // toc: true, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
